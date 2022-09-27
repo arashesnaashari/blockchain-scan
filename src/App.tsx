27ba2a90cff2cc77e5072489691cc8e598443ada
Hamidreza Nazemi <32125441+Hamidrezana@users.noreply.github.com>
2022-09-28 00:15:10 +0330

    fix: some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React from "react";
-import { useData, useMaker } from "./hooks";
+import React from 'react';
+import { useData, useMaker } from './hooks';
 
 export interface AppProps {}
 
 const App: React.FunctionComponent<AppProps> = () => {
-  const {
-    accounts,
-    transactions,
-    blocks,
-    addNewAccount,
-    addNewBlock,
-    addNewTransaction,
-  } = useData();
+  const { accounts, transactions, blocks, addNewAccount, addNewBlock, addNewTransaction } =
+    useData();
   useMaker({
     accounts,
     onMakeAccount: addNewAccount,
@@ -21,8 +15,10 @@ const App: React.FunctionComponent<AppProps> = () => {
 
   return (
     <div>
-      {accounts.map((el) => (
-        <p key={el.address}>{el.address} {el.balance}</p>
+      {accounts.map(el => (
+        <p key={el.address}>
+          {el.address} {el.balance}
+        </p>
       ))}
     </div>
   );
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
-export { default as useInterval } from "./useInterval";
-export { default as useData } from "./useData";
-export { default as useMaker } from "./useMaker";
+export { default as useInterval } from './useInterval';
+export { default as useData } from './useData';
+export { default as useMaker } from './useMaker';
