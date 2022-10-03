c8b80218678bc594604cd59b985cb7903bc32c6c
Hamidreza Nazemi <32125441+Hamidrezana@users.noreply.github.com>
2022-10-03 21:50:08 +0330

    feat: add memo

--- src/components/commons/Accounts/index.tsx
+++ src/components/commons/Accounts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import clsx from 'clsx';
 import { IAccount } from '../../../types';
 import Box from '../../cubes/Box';
@@ -8,26 +8,31 @@ export interface AccountsProps {
   accounts: Array<IAccount>;
 }
 
-const Accounts: React.FunctionComponent<AccountsProps> = ({ accounts }) => {
-  return (
-    <Box title="Latest Accounts">
-      <ul className="list-group list-group-flush">
-        {accounts.map(el => (
-          <li
-            className={clsx('list-group-item d-flex justify-content-between align-items-center', {
-              disabled: el.isSuspend,
-            })}
-            key={el.address}
-          >
-            <p className="mb-0">
-              {el.isSuspend ? <s>{fitString(el.address, 8)}</s> : fitString(el.address, 8)}
-            </p>
-            <span className="badge bg-primary rounded-pill">{el.balance}</span>
-          </li>
-        ))}
-      </ul>
-    </Box>
-  );
-};
+const Accounts: React.FunctionComponent<AccountsProps> = memo<AccountsProps>(
+  ({ accounts }) => {
+    return (
+      <Box title="Latest Accounts">
+        <ul className="list-group list-group-flush">
+          {accounts.map(el => (
+            <li
+              className={clsx('list-group-item d-flex justify-content-between align-items-center', {
+                disabled: el.isSuspend,
+              })}
+              key={el.address}
+            >
+              <p className="mb-0">
+                {el.isSuspend ? <s>{fitString(el.address, 8)}</s> : fitString(el.address, 8)}
+              </p>
+              <span className="badge bg-primary rounded-pill">{el.balance}</span>
+            </li>
+          ))}
+        </ul>
+      </Box>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.accounts.length === nextProps.accounts.length;
+  },
+);
 
 export default Accounts;
